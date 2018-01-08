d5feff68c47e5bd49481603abf23d5cbdd8cb22c
Tung Nguyen <kenkz447@gmail.com>
2018-01-08 22:35:01 +0700

    Init production

---
+++ app.js
@@ -0,0 +1,21 @@
+﻿'use strict';
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var publicFolder = path.join(__dirname, 'public')
+
+var app = express();
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(express.static(publicFolder));
+
+app.all('/*', function(req, res) {
+  res.sendfile('index.html', {root: publicFolder});
+})
+
+app.set('port', process.env.PORT || 3000);
+
+var server = app.listen(app.get('port'), function() {
+    console.log('Express server listening on port ' + server.address().port);
+});
